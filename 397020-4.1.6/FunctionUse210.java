df003e8abd30cff8791b87e988b92ddd4fb41b80
duzhg56980 <guokey1996@gmail.com>
2024-02-05 17:43:10 +0800

    init commit

---
+++ 397020-4.1.6/FunctionUse210.java
@@ -0,0 +1,6 @@
+import cn.hutool.http.HttpResponse;
+public class FunctionUse210 {
+public void funcUse() {
+HttpResponse httpresponse = new HttpResponse();
+}
+}
---
+++ 397020-4.1.6/FunctionUse280.java
@@ -0,0 +1,7 @@
+import cn.hutool.http.ssl.SSLSocketFactoryBuilder;
+public class FunctionUse280 {
+public void funcUse() {
+SSLSocketFactoryBuilder sslsocketfactorybuilder = new SSLSocketFactoryBuilder();
+sslsocketfactorybuilder.setTrustManagers(null);
+}
+}
